b8f7ba32e4d7f4fafb392924fe1569fdc05a95b0
flyingxu <flyingxu@gmail.com>
2017-04-18 15:17:35 +0800

    Add the missing forms pages. (copied from upstream https://github.com/XAM-Consulting/MvvmCrossNavigationDemo/tree/master/MvvmCrossNavigationDemo/XamarinPages)

---
+++ MvvmCrossNavigationDemo/FormsPages/SecondPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MvvmCrossNavigationDemo.Core
+{
+	public partial class SecondPage : ContentPage
+	{
+		public SecondPage ()
+		{
+            InitializeComponent();
+			
+		}
+	}
+}
+
